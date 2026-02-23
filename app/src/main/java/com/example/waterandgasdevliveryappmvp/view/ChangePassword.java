fead3e3488be4c685f86df838fd72d580f4de8ed
Fuoco <joshuaorlina08@gmail.com>
2026-02-23 11:32:04 +0800

    Add new ChangePassword Last

--- app/src/main/java/com/example/waterandgasdevliveryappmvp/view/ChangePassword.java
+++ app/src/main/java/com/example/waterandgasdevliveryappmvp/view/ChangePassword.java
@@ -1,6 +1,10 @@
 package com.example.waterandgasdevliveryappmvp.view;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +14,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.waterandgasdevliveryappmvp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class ChangePassword extends AppCompatActivity {
+    Button confirm;
+    EditText password, confirm_password;
+
+    ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_change_password);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //BACKBUTTON
+        back = findViewById(R.id.back_arrow3);
+        back.setOnClickListener(view -> {
+            finish();
+        });
+
+        //CONFIRM
+        confirm = findViewById(R.id.login_button);
+        confirm.setOnClickListener(view -> {
+
         });
     }
 }
